f2d0682a2846eb51b69b5039d45d91449f129b7b
NahidRuhan <dilal328@gmail.com>
2026-05-25 03:21:20 +0600

    implemented get api endpoint and stripe payment gateway

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const dns = require('dns');
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const dns = require("dns");
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
+const crypto = require("crypto");
 
 // Force Node.js to use Google DNS internally, bypassing stubborn router/antivirus blocks!
-dns.setServers(['8.8.8.8', '8.8.4.4']);
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
-require('dotenv').config()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 // middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+
+const generateTrackingId = () => {
+  const prefix = "spread-fast";
+  const date = new Date().toISOString().slice(0, 10).replace(/-/g, "");
+  const number = crypto.randomBytes(4).toString("hex").toUpperCase();
+  return `${prefix}-${date}-${number}`;
+};
 
 const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster1.uxgjftg.mongodb.net/?appName=Cluster1`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -21,48 +30,126 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
-
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const db = client.db('spread_fast_db')
-    const parcelsCollection = db.collection('parcels')
+    const db = client.db("spread_fast_db");
+    const parcelsCollection = db.collection("parcels");
+    const paymentCollection = db.collection("payments");
 
     // parcel related api
 
-    app.get('/parcels',async(req,res)=>{
-      
-      const query = {}
-      const {email} = req.query
-      if(email) query.senderEmail = email
+    app.get("/parcels", async (req, res) => {
+      const query = {};
+      const { email } = req.query;
+      if (email) query.senderEmail = email;
+      const options = { sort: { created_at: -1 } };
 
-      const cursor = parcelsCollection.find(query)
-      const result = await cursor.toArray()
-      res.send(result)
-    })
+      const cursor = parcelsCollection.find(query, options);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-    app.post('/parcels',async(req,res)=>{
-        const parcel = req.body
-        const result = await parcelsCollection.insertOne(parcel)
-        res.send(result)
-    })
+    app.get("/parcels/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await parcelsCollection.findOne(query);
+      res.send(result);
+    });
 
+    app.post("/parcels", async (req, res) => {
+      const parcel = req.body;
+      parcel.created_at = new Date();
+      const result = await parcelsCollection.insertOne(parcel);
+      res.send(result);
+    });
 
+    app.delete("/parcels/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await parcelsCollection.deleteOne(query);
+      res.send(result);
+    });
 
+    // payment gateway API
+    app.post("/payment-checkout-session", async (req, res) => {
+      const paymentInfo = req.body;
+      const amount = parseInt(paymentInfo.deliveryCharge) * 100;
+      const session = await stripe.checkout.sessions.create({
+        line_items: [
+          {
+            price_data: {
+              currency: "usd",
+              unit_amount: amount,
+              product_data: {
+                name: `Please pay for ${paymentInfo.parcelName}`,
+              },
+            },
+            quantity: 1,
+          },
+        ],
+        mode: "payment",
+        metadata: {
+          parcelId: paymentInfo.parcelId,
+          parcelName: paymentInfo.parcelName,
+        },
+        customer_email: paymentInfo.senderEmail,
+        success_url: `${process.env.SITE_DOMAIN}/dashboard/payment-success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${process.env.SITE_DOMAIN}/dashboard/payment-cancelled`,
+      });
+      res.send({ url: session.url });
+    });
 
+    app.patch("/payment-success", async (req, res) => {
+      const sessionId = req.query.session_id;
+      const session = await stripe.checkout.sessions.retrieve(sessionId);
+      const trackingId = generateTrackingId();
+      if (session.payment_status === "paid") {
+        const id = session.metadata.parcelId;
+        const query = { _id: new ObjectId(id) };
+        const update = {
+          $set: {
+            paymentStatus: session.payment_status,
+            trackingId: trackingId,
+          },
+        };
+        const result = await parcelsCollection.updateOne(query, update);
 
+        const payment = {
+          amount: session.amount_total / 100,
+          currency: session.currency,
+          customerEmil: session.customer_email,
+          parcelId: session.metadata.parcelId,
+          parcelName: session.metadata.parcelName,
+          transactionId: session.payment_intent,
+          paymentStatus: session.payment_status,
+          paid_at: new Date(),
+        };
 
+        const resultPayment = await paymentCollection.insertOne(payment);
 
-
+        res.send({
+          success: true,
+          modifyParcel: result,
+          trackingId: trackingId,
+          transactionId: session.payment_intent,
+          paymentInfo: resultPayment,
+        });
+        return;
+      }
+      res.send({ success: false });
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -70,12 +157,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-app.get('/', (req, res) => {
-  res.send('Spread fast!!!!')
-})
+app.get("/", (req, res) => {
+  res.send("Spread fast!!!!");
+});
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`)
-})
+  console.log(`Server is running at http://localhost:${port}`);
+});
